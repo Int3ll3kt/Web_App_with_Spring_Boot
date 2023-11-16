e776fa6ac771457ff33039c2252354ef4a1df6c2
int3ll3kt <int3ll3kt@gmail.com>
2023-11-16 08:43:42 +0100

    Initial commit

---
+++ src/main/java/com/springbootlearning/learningspringboot3/ch2/Chapter2Application.java
@@ -0,0 +1,17 @@
+package com.springbootlearning.learningspringboot3.ch2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@SpringBootApplication
+public class Chapter2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter2Application.class, args);
+	}
+
+}
+
+
+
